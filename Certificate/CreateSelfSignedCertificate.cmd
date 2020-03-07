96f0dac7d9404fe4a4aa7f96605769040502954c
Raphael Hinterndorfer <raphael.hinterndorfer@outlook.at>
2020-03-07 21:19:42 +0100

    Remove ClickOnce

--- Certificate/CreateSelfSignedCertificate.cmd
+++
@@ -1,5 +0,0 @@
-cd "C:\Program Files (x86)\Windows Kits\10\bin\x86"
-echo password: !w4cash!
-makecert -r -pe -n "CN=www.hb-softsolution.com" C:\Work\GitHub\w4cashPortal\Certificate\w4cash.cer -b 01/01/2016 -e 01/01/2099 -a sha256 -cy end -sky signature -ss my -sv C:\Work\GitHub\w4cashPortal\Certificate\w4cash.pvk
-pvk2pfx -pvk C:\Work\GitHub\w4cashPortal\Certificate\w4cash.pvk -pi !w4cash! -spc C:\Work\GitHub\w4cashPortal\Certificate\w4cash.cer -pfx C:\Work\GitHub\w4cashPortal\Certificate\w4cash.pfx -po !w4cash! -f
-pause
--- w4cashPortal/encodedecode.ps1
+++
@@ -1,2 +0,0 @@
-﻿[Convert]::ToBase64String([System.Text.UnicodeEncoding]::UTF8.GetBytes("nd1ub26x"));
-[System.Text.UnicodeEncoding]::UTF8.GetString([Convert]::FromBase64String("c2lsZW50"));
